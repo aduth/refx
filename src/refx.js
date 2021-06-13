9030d40eeb21baec9b1b34fb32132fb5e480930c
Andrew Duthie <andrew@andrewduthie.com>
2021-06-13 16:50:22 -0400

    Migrate to personal ESLint configuration

--- src/refx.js
+++ src/refx.js
@@ -1,34 +1,35 @@
-function flattenIntoMap( map, effects ) {
+function flattenIntoMap(map, effects) {
 	var i;
-	if ( Array.isArray( effects ) ) {
-		for ( i = 0; i < effects.length; i++ ) {
-			flattenIntoMap( map, effects[ i ] );
+	if (Array.isArray(effects)) {
+		for (i = 0; i < effects.length; i++) {
+			flattenIntoMap(map, effects[i]);
 		}
 	} else {
-		for ( i in effects ) {
-			map[ i ] = ( map[ i ] || [] ).concat( effects[ i ] );
+		for (i in effects) {
+			map[i] = (map[i] || []).concat(effects[i]);
 		}
 	}
 }
 
-function refx( effects ) {
+function refx(effects) {
 	var map = {},
 		middleware;
 
-	flattenIntoMap( map, effects );
+	flattenIntoMap(map, effects);
 
-	middleware = function( store ) {
-		return function( next ) {
-			return function( action ) {
-				var handlers = map[ action.type ],
-					result = next( action ),
-					i, handlerAction;
+	middleware = function (store) {
+		return function (next) {
+			return function (action) {
+				var handlers = map[action.type],
+					result = next(action),
+					i,
+					handlerAction;
 
-				if ( handlers ) {
-					for ( i = 0; i < handlers.length; i++ ) {
-						handlerAction = handlers[ i ]( action, store );
-						if ( handlerAction ) {
-							store.dispatch( handlerAction );
+				if (handlers) {
+					for (i = 0; i < handlers.length; i++) {
+						handlerAction = handlers[i](action, store);
+						if (handlerAction) {
+							store.dispatch(handlerAction);
 						}
 					}
 				}
--- test/refx.js
+++ test/refx.js
@@ -1,17 +1,17 @@
 var sinon, refx;
 
-sinon = require( 'sinon' );
-refx = require( '../' );
+sinon = require('sinon');
+refx = require('../');
 
-describe( 'refx()', function() {
+describe('refx()', function () {
 	var effects = {
-		TEST: function( action, store ) {
-			store.dispatch( {
+		TEST: function (action, store) {
+			store.dispatch({
 				type: action.type + '_EFFECTED',
 				data: store.getState(),
-			} );
+			});
 		},
-		TEST_RETURN: function( action, store ) {
+		TEST_RETURN: function (action, store) {
 			return {
 				type: action.type + '_EFFECTED',
 				data: store.getState(),
@@ -19,60 +19,60 @@ describe( 'refx()', function() {
 		},
 	};
 
-	function assert( middleware ) {
+	function assert(middleware) {
 		var spy, store;
 
 		spy = sinon.spy();
 		store = {
 			dispatch: spy,
-			getState: function() {
+			getState: function () {
 				return true;
 			},
 		};
 
-		middleware( store )( spy )( { type: 'TEST' } );
-		middleware( store )( spy )( { type: 'TEST_RETURN' } );
+		middleware(store)(spy)({ type: 'TEST' });
+		middleware(store)(spy)({ type: 'TEST_RETURN' });
 
 		// Validate ignoring prototype members
-		middleware( store )( function() {} )( { type: 'valueOf' } );
+		middleware(store)(function () {})({ type: 'valueOf' });
 
-		sinon.assert.callCount( spy, 4 );
-		sinon.assert.match( spy.getCall( 0 ).args[ 0 ], {
+		sinon.assert.callCount(spy, 4);
+		sinon.assert.match(spy.getCall(0).args[0], {
 			type: 'TEST',
-		} );
-		sinon.assert.match( spy.getCall( 1 ).args[ 0 ], {
+		});
+		sinon.assert.match(spy.getCall(1).args[0], {
 			type: 'TEST_EFFECTED',
 			data: true,
-		} );
-		sinon.assert.match( spy.getCall( 2 ).args[ 0 ], {
+		});
+		sinon.assert.match(spy.getCall(2).args[0], {
 			type: 'TEST_RETURN',
-		} );
-		sinon.assert.match( spy.getCall( 3 ).args[ 0 ], {
+		});
+		sinon.assert.match(spy.getCall(3).args[0], {
 			type: 'TEST_RETURN_EFFECTED',
 			data: true,
-		} );
+		});
 	}
 
-	it( 'should accept an object of keys', function() {
-		var middleware = refx( effects );
+	it('should accept an object of keys', function () {
+		var middleware = refx(effects);
 
-		assert( middleware );
-	} );
+		assert(middleware);
+	});
 
-	it( 'should accept an array of objects of keys', function() {
-		var middleware = refx( [ effects ] );
+	it('should accept an array of objects of keys', function () {
+		var middleware = refx([effects]);
 
-		assert( middleware );
-	} );
+		assert(middleware);
+	});
 
-	it( 'should allow after-the-fact manipulation via effects property', function() {
-		var middleware = refx( [] ),
+	it('should allow after-the-fact manipulation via effects property', function () {
+		var middleware = refx([]),
 			actionType;
 
-		for ( actionType in effects ) {
-			middleware.effects[ actionType ] = [ effects[ actionType ] ];
+		for (actionType in effects) {
+			middleware.effects[actionType] = [effects[actionType]];
 		}
 
-		assert( middleware );
-	} );
-} );
+		assert(middleware);
+	});
+});
